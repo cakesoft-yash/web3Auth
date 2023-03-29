809a284cfb5950a46a9b500422e81acaa4e64751
yash <yash@cakesofttech.com>
2023-03-29 11:44:52 +0530

    1.Initial commit.

---
+++ app.js
@@ -0,0 +1,45 @@
+const ethers = require('ethers');
+const express = require('express');
+
+const app = express();
+const port = process.env.port || 3000;
+
+// app.set('view engine', 'ejs');
+// app.use('/jsFile', express.static('./views/functions'))
+
+app.get('/', (req, res) => res.send(new Date().toString()));
+
+app.get('/getSignMessage', async (req, res) => res.json(
+    {
+        success: true,
+        messageToSign: 'Sign Message'
+    }
+));
+
+app.get('/verifySignMessage', async (req, res) => {
+    try {
+        if (!req.query.messageToSign) throw Error('Message is required');
+        if (!req.query.signature) throw Error('Signature is required');
+        let userAddress = await ethers.utils.verifyMessage(req.query.messageToSign, req.query.signature);
+        if (userAddress) {
+            res.json(
+                {
+                    address: userAddress,
+                    signature: req.query.signature,
+                    signedMessage: req.query.messageToSign
+                }
+            );
+        }
+    } catch (error) {
+        res.json(
+            {
+                success: false,
+                error: error.message
+            }
+        );
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at ${port}`);
+});
